820c1e5383b24c89d09fcd3ca8b59557c2b33baa
zhangguokai <zhangguokai@inspur.com>
2024-12-15 09:58:25 +0800

    feat: 重写ORM结构

--- src/resp.rs
+++ src/resp.rs
@@ -10,30 +10,41 @@ where
     pub data: Option<T>,
 }
 
-impl<T> RespVo<T>
-where
-    T: Serialize + Clone + Default + Send + Sync,
-{
-    pub fn query_ok(data: T, msg: &str) -> RespVo<T> {
-        RespVo {
-            code: "0".to_string(),
-            data: Some(data),
-            msg: msg.to_string(),
-        }
-    }
-    pub fn query_op_ok(data: Option<T>, msg: &str) -> RespVo<T> {
-        RespVo {
-            code: "0".to_string(),
-            data,
-            msg: msg.to_string(),
-        }
-    }
-}
+impl<T> RespVo<T> where T: Serialize + Clone + Default + Send + Sync {}
 
 impl<T> RespVo<T>
 where
     T: Serialize + Clone + Default + Send + Sync,
 {
+    pub fn query_ok(data: T) -> RespVo<T> {
+        RespVo {
+            code: "0".to_string(),
+            data: Some(data),
+            msg: "查询成功".to_string(),
+        }
+    }
+    pub fn query_op_ok(data: Option<T>) -> RespVo<T> {
+        RespVo {
+            code: "0".to_string(),
+            data,
+            msg: "查询成功".to_string(),
+        }
+    }
+    pub fn save_ok(data: T) -> RespVo<T> {
+        RespVo {
+            code: "0".to_string(),
+            data: Some(data),
+            msg: "保存成功".to_string(),
+        }
+    }
+    pub fn save_op_ok(data: Option<T>) -> RespVo<T> {
+        RespVo {
+            code: "0".to_string(),
+            data,
+            msg: "保存成功".to_string(),
+        }
+    }
+
     pub fn ok_msg<M>(data: T, msg: M) -> RespVo<T>
     where
         M: ToString,
@@ -41,6 +52,16 @@ where
         RespVo {
             code: "0".to_string(),
             data: Some(data),
+            msg: msg.to_string(),
+        }
+    }
+    pub fn ok_op_msg<M>(data: Option<T>, msg: M) -> RespVo<T>
+    where
+        M: ToString,
+    {
+        RespVo {
+            code: "0".to_string(),
+            data,
             msg: msg.to_string(),
         }
     }
